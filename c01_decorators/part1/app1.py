01a6ab0033fa6096ac218bb586dd8c2141a6e123
emanuel.craciun <emanuel.craciun@icloud.com>
2023-03-08 21:05:42 +0200

    app1

--- c01_decorators/part1/app1.py
+++ c01_decorators/part1/app1.py
@@ -6,48 +6,49 @@
 
 import datetime
 import time
-from datetime import datetime
 from functools import wraps
 
 
 def working_hours(start, stop):
+    start = datetime.time(hour=start)
+    stop = datetime.time(hour=stop)
+
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             if time.localtime().tm_hour not in range(start.hour, stop.hour):
-                return
-            result = func(*args, **kwargs)
-            return result
+                pass
+            else:
+                return func(*args, **kwargs)
 
         return wrapper
 
     return decorator
 
 
-@working_hours(datetime.time(hour=9), datetime.time(hour=22))
+@working_hours(9, 22)
 def alarm():
     print("Wack up")
 
 
 alarm()
 
-
-def inception(begin=9, end=17):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            if datetime.now().hour not in range(begin, end):
-                return None
-            func(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
-
-
-@inception(9, 21)
-def alarm():
-    print("Wake up!")
-
-
-alarm()
+# def inception(begin=9, end=17):
+#     def decorator(func):
+#         @wraps(func)
+#         def wrapper(*args, **kwargs):
+#             if datetime.now().hour not in range(begin, end):
+#                 return None
+#             func(*args, **kwargs)
+#
+#         return wrapper
+#
+#     return decorator
+#
+#
+# @inception(9, 21)
+# def alarm():
+#     print("Wake up!")
+#
+#
+# alarm()
