2fcf3c50a55d40c1fc57f9a3f2a9968a5a5683f7
emanuel.craciun <emanuel.craciun@icloud.com>
2023-03-25 17:03:41 +0200

    04

--- c4_timeit/app3.py
+++ c4_timeit/app3.py
@@ -17,43 +17,47 @@ def ip_information(os_type):
     obj = os.popen(ip_command)
     output = obj.read()
     pattern = r'\s+IPv4 Address(\.\s)+:\s*(?P<IPaddress>\d+\.\d+\.\d+\.\d+)'
-    return re.search(pattern, output).group('IPaddress')
 
-    # try:
-    #     yield obj
-    # except Exception as e:
-    #     print(e.args)
-    # finally:
-    #     print("end")
+    try:
+        print('abcd')
+        yield re.search(pattern, output).group('IPaddress')
+    except Exception as e:
+        print(e.args)
+    finally:
+        print("end")
 
 
 if __name__ == '__main__':
     # print(ip_information("nt"))
     with ip_information("nt") as ip1:
-        print(ip1.read())
+        print(ip1)
+        raise AttributeError("my_error")
+        print(ip1)
 
-
-class My_File_Opener:
-
-    def __init__(self, file_name):
-        self.file = open(file_name, 'r')
-
-    def __enter__(self):
-        print('''in enter''')
-
-    def __next__(self):
-        pass
-
-    def __iter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        print('''in exit''')
-
-
-
-# mfo = My_File_Opener('part1.py')
-# mfo.__enter__()
-
-with My_File_Opener('part1.py') as mfo:
-    print('running')
+#
+# class My_File_Opener:
+#
+#     def __init__(self, file_name):
+#         self.file = open(file_name, 'r')
+#
+#     def __enter__(self):
+#         print('''in enter''')
+#         return self
+#
+#     def __next__(self):
+#         pass
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __exit__(self, exc_type, exc_val, exc_tb):
+#         print('''in exit''')
+#
+#
+#
+# # mfo = My_File_Opener('part1.py')
+# # mfo.__enter__()
+#
+# with My_File_Opener('part1.py') as mfo:
+#     print('running')
+#     print(mfo)
