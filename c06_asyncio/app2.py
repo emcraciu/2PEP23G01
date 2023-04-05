af312e98bccaab4db949b1b0015335724e8de102
emanuel.craciun <emanuel.craciun@icloud.com>
2023-04-05 20:17:23 +0300

    step2

--- c06_asyncio/app2.py
+++ c06_asyncio/app2.py
@@ -7,3 +7,33 @@ http://worldtimeapi.org/api/timezone/
 
 this should be obtained as fast as possible
 """
+
+import aiohttp
+import json
+import time
+import asyncio
+
+
+async def regions(session):
+    response = await session.request(method='GET', url=f'http://worldtimeapi.org/api/timezone/')
+    text = await response.text()
+    locations = json.loads(text)
+    return set(map(lambda a: a.split('/')[0], locations))
+
+
+async def time_zone(session, region):
+    response = await session.request(method='GET', url=f'http://worldtimeapi.org/api/timezone/{region}')
+    text = await response.text()
+    time_zones = json.loads(text)
+    if isinstance(time_zones, list):
+        return set(map(lambda a: a.split("/")[1], time_zones))
+
+
+async def get_regions():
+    async with aiohttp.ClientSession() as session:
+        task = await asyncio.gather(regions(session), time_zone(session, "America"))
+        print(task[1])
+
+
+if __name__ == "__main__":
+    asyncio.run(get_regions())
