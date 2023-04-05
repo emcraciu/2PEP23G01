79f16ff80e72cdb0255c97381698cae1eeb1d9d0
emanuel.craciun <emanuel.craciun@icloud.com>
2023-04-05 22:34:09 +0300

    part7

---
+++ c07_multiprocessing/part1.py
@@ -0,0 +1,21 @@
+from multiprocessing import Pool
+import json
+from time import sleep
+
+import requests
+
+def time_zone(region):
+    sleep(5)
+    response = requests.get(f'http://worldtimeapi.org/api/timezone/{region}')
+    text = response.text
+    time_zones = json.loads(text)
+    if isinstance(time_zones, list):
+        return set(map(lambda a: a.split("/")[1], time_zones))
+
+
+# time_zone('Africa')
+
+
+if __name__ == '__main__':
+    with Pool(10) as p:
+        print(p.map(time_zone, ['Africa', 'America', 'Europa']))
---
+++ c07_multiprocessing/part2.py
@@ -0,0 +1,30 @@
+from multiprocessing import Process
+import json
+import time
+from time import sleep
+
+import requests
+
+def time_zone(region):
+    sleep(3)
+    response = requests.get(f'http://worldtimeapi.org/api/timezone/{region}')
+    text = response.text
+    time_zones = json.loads(text)
+    if isinstance(time_zones, list):
+        return set(map(lambda a: a.split("/")[1], time_zones))
+
+print(__name__)
+
+if __name__ == '__main__':
+    start = time.time()
+    processes = []
+    for region in ['Africa', 'America', 'Europa']:
+        p = Process(target=time_zone, args=(region,))
+        p.start()
+        processes.append(p)
+    sleep(5)
+    print('running')
+    for p in processes:
+        p.join()
+    end = time.time()
+    print(end-start)
