91a2629dc3a3b4d7bbd1e78147e7c2b5d790e891
emanuel.craciun <emanuel.craciun@icloud.com>
2023-04-07 20:06:28 +0300

    part7

--- c07_multiprocessing/part3.py
+++ c07_multiprocessing/part3.py
@@ -1,24 +1,48 @@
-import time
 from multiprocessing import Process, Queue
-
-def f(q:Queue):
-    time.sleep(10)
-    q.put([42, None, 'hello'])
-    print('done', __name__, f.__name__)
-
-def d(q:Queue):
-    # while q.empty():
-    #     time.sleep(0.1)
-    print(q.get(timeout=5), __name__, d.__name__)
+import json
+import time
+from time import sleep
+import requests
 
 
+def region(region: Queue):
+    response = requests.get(f'http://worldtimeapi.org/api/timezone')
+    text = response.text
+    time_zones = json.loads(text)
+    if isinstance(time_zones, list):
+        for reg in set(map(lambda a: a.split("/")[0], time_zones)):
+            region.put(reg)
+
+
+def time_zone(region: Queue):
+    reg = region.get()
+    response = requests.get(f'http://worldtimeapi.org/api/timezone/{reg}')
+    text = response.text
+    time_zones = json.loads(text)
+    if isinstance(time_zones, list):
+        print(set(map(lambda a: a.split("/")[1], time_zones)))
+    else:
+        print("not list", time_zones)
+
+
+print(__name__)
 
 if __name__ == '__main__':
     q = Queue()
-    my_d = Process(target=d, args=(q,))
-    my_f = Process(target=f, args=(q,))
-    my_d.start()
-    my_f.start()
-    # print(q.get())    # prints "[42, None, 'hello']"
-    my_f.join()
-    my_d.join()
+    start = time.time()
+    processes = []
+    for _ in range(3):
+        x = Process(target=time_zone, args=(q,))
+        x.start()
+        processes.append(x)
+
+    p = Process(target=region, args=(q,))
+    p.start()
+    print('running')
+    time.sleep(10)
+
+    for y in processes:
+        y.join()
+    end = time.time()
+    p.join()
+    print(end - start)
